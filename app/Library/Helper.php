2ba1c164c443da8a059cde1c2aee19b8a4cde14e
程曦 <544480052@qq.com>
2018-03-30 18:45:59 +0800

    将公共方法放到app/Library中

---
+++ app/Library/Helper.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!function_exists('config')) {
+    /**
+     * 加载配置文件并返回配置文件内容
+     * 可以使用 . 符号获取配置文件中的key，支持递归获取子key
+     * @param $param
+     * @return mixed
+     */
+    function config($param)
+    {
+        $explode = explode('.', $param);
+        $fileName = array_shift($explode);
+        $file = ROOT . '/config/' . $fileName . '.php';
+        $globalKey = md5('config');
+        $globalSubKey = hash_hmac('md5', $fileName, $fileName);
+        if (isset($GLOBALS[$globalKey][$globalSubKey])) {
+            return empty($explode) ?
+                $GLOBALS[$globalKey][$globalSubKey]
+                :
+                get_deep_value($GLOBALS[$globalKey][$globalSubKey], implode('.', $explode));
+        } else if (file_exists($file)) {
+            $config = include_once $file;
+            if ($config === true) {
+                return false;
+            }
+            $GLOBALS[$globalKey][$globalSubKey] = $config;
+            return empty($explode) ?
+                $config
+                :
+                get_deep_value($config, implode('.', $explode));
+        }
+        return false;
+    }
+}
+
